9741e35b296a9f40a2f574b4f0e467e357883f12
Manya kalra <73202244+Manyakalra3@users.noreply.github.com>
2022-01-21 23:08:29 +0530

    Time: 1 ms (99.97%), Space: 60.4 MB (54.02%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,19 +1,18 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        
- int start = 0, currentBalance = 0, remainingBalance = 0;
-       // iterate through each gas station and update the starting point, which takes O(n) time
-        for (int i = 0; i < cost.length; i++) {
-            // update the current gas balance
-            currentBalance += gas[i] - cost[i];
-            // if the currently considering balance < 0, we can't move to the next gas station -> the current start is invalid
-            if (currentBalance < 0) {
-                start = i + 1; // update start to new candidate value
-                remainingBalance += currentBalance; // update the negative balance (of the remaining part)
-                currentBalance = 0; // reset the currently considering balance
+        int start=0,rem=0,curr=0;
+        for(int i=0;i<gas.length;i++){
+             curr+=gas[i]-cost[i];
+            if(curr<0){
+                start=i+1;
+                rem+=curr;
+                curr=0;
+                
             }
         }
-        // at the end, we check whether the positive balance (currentBalance) can compensate the remainingBalance (currentBalance + remainingBalance has to be >= 0)
-        return currentBalance + remainingBalance >= 0 ? start : -1;
+            if(curr+rem>=0){
+                return start;
             }
+        return -1;
+    }
 }
