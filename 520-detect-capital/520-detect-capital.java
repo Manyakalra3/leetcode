c9715ea14130778d7fbef72bc1729db1440891df
Manya kalra <73202244+Manyakalra3@users.noreply.github.com>
2022-01-24 10:37:20 +0530

    Time: 1 ms (99.35%), Space: 37.3 MB (78.54%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,48 +1,13 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int n = word.length();
-
-        boolean match1 = true, match2 = true, match3 = true;
-
-        // case 1: All capital
-        for (int i = 0; i < n; i++) {
-            if (!Character.isUpperCase(word.charAt(i))) {
-                match1 = false;
-                break;
+        
+        int caps = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))){
+                caps++;
             }
         }
-        if (match1) {
-            return true;
-        }
-
-        // case 2: All not capital
-        for (int i = 0; i < n; i++) {
-            if (!Character.isLowerCase(word.charAt(i))) {
-                match2 = false;
-                break;
-            }
-        }
-        if (match2) {
-            return true;
-        }
-
-        // case 3: All not capital except first
-        if (!Character.isUpperCase(word.charAt(0))) {
-            match3 = false;
-        }
-        if (match3) {
-            for (int i = 1; i < n; i++) {
-                if (!Character.isLowerCase(word.charAt(i))) {
-                    match3 = false;
-                    break;
-                }
-            }
-        }
-        if (match3) {
-            return true;
-        }
-
-        // if not matching
-        return false;
+        if (caps == word.length() || caps == 0) return true;
+        return caps == 1 && Character.isUpperCase(word.charAt(0));
     }
 }
