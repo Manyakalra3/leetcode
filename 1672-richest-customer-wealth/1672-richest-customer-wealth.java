5a6b547246b776056983aa7a149275c980346af8
Manya kalra <73202244+Manyakalra3@users.noreply.github.com>
2022-01-31 13:46:11 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (5.42%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int[]arr:accounts){
+            int sum=0;
+            for(int i:arr){
+                sum+=i;
+            }
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
+}
