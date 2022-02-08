ca35b476f2a9dbfd8316207571ff474559ab7268
Manya kalra <73202244+Manyakalra3@users.noreply.github.com>
2022-02-08 19:29:01 +0530

    Time: 1 ms (98.89%), Space: 39.8 MB (16.11%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        int result=0;
+    
+        while(num>0){
+            result+=num%10;
+            num=num/10;
+             if(num==0 && result>9){
+                 num=result;
+                 result=0;
+             }   
+        }
+        return result;
+    }
+}
